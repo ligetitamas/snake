1cb6d4cedaaf6c1d276da57e6bcbc9443aed77f7
ligeti.tamas <ltommipresents@gmail.com>
2021-02-25 09:56:55 +0100

    fej létrehozva

---
+++ snake.py
@@ -0,0 +1,26 @@
+import turtle
+import time
+
+
+def jobbra():
+    fej.right(90)
+
+
+palya = turtle.Screen()
+palya.setup(width=800, height=600)
+palya.bgcolor("green")
+palya.title("Snake")
+palya.tracer(0)
+palya.listen()
+palya.onkey(jobbra, "Right")
+
+fej = turtle.Turtle()
+fej.shape("triangle")
+fej.penup()
+fej.color("yellow")
+
+while True:
+    fej.forward(20)
+    palya.update()
+    time.sleep(0.3)
+
